2254cdd2bf5bf379278c46cc6bca872fd663bd73
Valerian Backeberg <valerian@backebergs.de>
2022-09-08 14:01:09 +0200

    Step 3.1 Icon Button Story

--- src/stories/IconButton.jsx
+++ src/stories/IconButton.jsx
@@ -1,22 +1,24 @@
-export const IconButton = ({
-  primary,
-  backgroundColor,
-  size,
-  label,
-  ...props
-}) => {
-  const mode = primary
-    ? "storybook-button--primary"
-    : "storybook-button--secondary";
+import PropTypes from 'prop-types';
 
+export const IconButton = ({ icon }) => {
   return (
     <button
       type="button"
-      className={["storybook-button", `storybook-button-${size}`, mode].join(
-        " "
-      )}
-      style={backgroundColor && { backgroundColor }}
-      {...props}
-    ></button>
+      className={[
+        'storybook-button',
+        'storybook-button-medium',
+        'storybook-button--primary',
+      ].join(' ')}
+    >
+      <span class="material-symbols-outlined">{icon}</span>
+    </button>
   );
+};
+
+IconButton.propTypes = {
+  icon: PropTypes.string,
+};
+
+IconButton.defaultProps = {
+  icon: 'search',
 };
--- src/stories/IconButton.stories.jsx
+++ src/stories/IconButton.stories.jsx
@@ -1,3 +1,13 @@
+import { IconButton } from './IconButton';
+
 export default {
-  title: "Example/IconButton",
+  title: 'Example/IconButton',
+  component: IconButton,
+};
+
+const Template = (args) => <IconButton {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  icon: 'search',
 };
